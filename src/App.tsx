4354be1da938cfdcbf2660a40e896546d1d970aa
Yuya Shintani <yuya.shintani.uplus26e7@gmail.com>
2021-02-17 01:02:45 +0900

    🚧 Get Users from mock server

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getUsers } from './services/user-service';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers().then((data) => setUsers(data));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Users from JSON Placeholder</h1>
+      <code>{JSON.stringify(users)}</code>
     </div>
   );
 }
